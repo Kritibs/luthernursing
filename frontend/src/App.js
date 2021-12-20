7dcc7ca1163a238ef494dd639eb87e988d8730b4
Kritib Bhattarai <bhatkr01@kritibs-mbp.lc.luther.edu>
2021-12-20 10:18:29 -0600

    after deletion

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ResponsiveAppBar from './components/ResponsiveAppBar.js';
+
+function App() {
+  return (
+    <div className="App">
+	  <ResponsiveAppBar />
+	  
+    </div>
+  );
+}
+
+export default App;

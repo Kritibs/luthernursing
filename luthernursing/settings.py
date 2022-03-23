04a96723851fc676c0028d7aac2a152dec7372f2
Kritib Bhattarai <bhatkr01@Kritibs-MacBook-Pro.local>
2022-03-23 17:54:21 -0500

    trail

--- luthernursing/settings.py
+++ luthernursing/settings.py
@@ -209,7 +209,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),

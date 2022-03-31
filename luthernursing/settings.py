8302f561421e1a6f38b5b2679ada815df103c39e
Kritib Bhattarai <bhatkr01@Kritibs-MacBook-Pro.local>
2022-03-30 22:00:54 -0500

    updated some settigns for img

--- luthernursing/settings.py
+++ luthernursing/settings.py
@@ -144,11 +144,18 @@ MEDIA_URL='/media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS= (os.path.join(BASE_DIR, 'build/static'),)
+STATICFILES_DIRS= (
+  os.path.join(BASE_DIR, 'build/static'),
+  os.path.join(BASE_DIR, 'build/media'),
+  os.path.join(BASE_DIR, 'media/images')
+  )
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

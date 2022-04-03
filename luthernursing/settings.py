af5581da4c73c1124ef381ad44dbccdbddddd296
Kritib Bhattarai <bhatkr01@Kritibs-MacBook-Pro.local>
2022-04-03 17:48:57 -0500

    email 6

--- luthernursing/settings.py
+++ luthernursing/settings.py
@@ -200,7 +200,7 @@ AUTHENTICATION_BACKENDS=(
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_USER = "luthernursingclub@gmail.com"
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS=True
 
